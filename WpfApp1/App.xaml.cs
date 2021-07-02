0ebe14b411901f9d0622911156d5753afe130cca
rfuzzo <r.fuzzo@gmail.com>
2021-07-02 22:52:27 +0200

    delet stuff

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -5,9 +5,6 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using ReactiveUI;
-using Splat;
-using WolvenManager.App.Services;
 
 namespace WpfApp1
 {
@@ -19,11 +16,7 @@ namespace WpfApp1
 
         public App()
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDM4Njc4QDMxMzkyZTMxMmUzMG5SV05xYWpUK3lBc3RKZE0vNnJsK09qYmx6YWppRmlFeEZlcjcwSnF2L0E9");
-
-
-            Locator.CurrentMutable.RegisterConstant(new WatcherService(), typeof(IWatcherService));
-            Locator.CurrentMutable.Register(() => new MainWindow(), typeof(IViewFor<AppViewModel>));
+           
 
         }
 
